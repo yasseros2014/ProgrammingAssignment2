c75802082c3ae8d46e9e0d1af824efb17fdb1d60
yasser <yasser.idrissi@gmail.com>
2014-04-26 21:01:36 +0000

    commit done

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##  A short comment describing makeCacheMatrix function
+##
+## this function has a list as an output, the following are the four functions
+## -setM(): its role is to define the Matrix. The role of <<- operator
+##         trigger a search strating from the parent environment
+## -getM(): its role is to memorize the Matrix and call it as needed
+## -setIn(): use to cache the Inverse of the matrix
+## -getIn(): use to call the inverse of the matrix as needed
 
 makeCacheMatrix <- function(x= matrix()){
         i <- NULL
@@ -17,7 +22,17 @@ makeCacheMatrix <- function(x= matrix()){
              getIn = getIn)
 }
 
-## Write a short comment describing this function
+
+## A short comment describing cacheSolve function
+##
+## This function has two mean roles :
+## 1- it check if the invers of the matrix is all readly cached or not 
+##by calling getIn() and checking the ouput
+##
+##2- it calculate and return the inverse of the matrix, also it cache the invers via setIn()
+##function
+##
+
 
 cacheSolve <- function(x,...) {
         i <-x$getIn()
